9a605e4a9feff36cd2dc9212a28893c844e45af8
Miha Rebernik <rasmajx@gmail.com>
2009-12-29 00:58:24 +0100

    Adding image editing capatibility.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,8 @@
 class ImagesController < ResourceController::Base
   belongs_to :spot
+
+  create.wants.html { redirect_to @spot }
+  update.wants.html { redirect_to @spot }
+  destroy.wants.html { redirect_to @spot }
+  
 end
