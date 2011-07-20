17371a2255ab2ebe242262fd668345c9836ad5c2
Miha Rebernik <rasmajx@gmail.com>
2011-07-20 10:17:19 -0700

    Fixed some translations, making it possible to edit multi language fields.

--- app/controllers/spot_infos_controller.rb
+++ app/controllers/spot_infos_controller.rb
@@ -1,9 +1,3 @@
 class SpotInfosController < ResourceController::Base
   belongs_to :spot
-  
-  protected
-  
-  def parent_object
-    @spot ||= Spot.find_by_name_websafe params[:spot_id]
-  end
 end
