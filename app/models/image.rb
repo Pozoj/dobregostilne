b87aabc10eb71a04b153db0d276d24b535c966fc
Miha Rebernik <miha@rebernik.info>
2013-09-22 22:28:09 -0700

    Fixing.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :spot
   has_attached_file :image, :styles => { :mini => "50x50#", :square => "75x75#", :small => "150x150", :medium => "320x320>", :normal => "550x550>" },
                     :url  => "/assets/spots/:id/:style/:basename.:extension",
+                    :path  => "/assets/spots/:id/:style/:basename.:extension",
                     :whiny => false,
                     :storage => :s3,
                     :bucket => ENV['AWS_BUCKET'],
