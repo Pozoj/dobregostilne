ee9699fae53c9e7767c5930601eeebe3af8af24e
Miha Rebernik <miha@rebernik.info>
2013-09-23 09:54:33 -0700

    More fog stuff.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,13 +1,11 @@
 class Image < ActiveRecord::Base
   belongs_to :spot
   has_attached_file :image, :styles => { :mini => "50x50#", :square => "75x75#", :small => "150x150", :medium => "320x320>", :normal => "550x550>" },
-                    :url  => "/assets/spots/:id/:style/:basename.:extension",
                     :path  => "/assets/spots/:id/:style/:basename.:extension",
                     :whiny => false,
                     :storage => :s3,
                     :bucket => ENV['AWS_BUCKET'],
                     :s3_credentials => {
-                      :bucket => ENV['AWS_BUCKET'],
                       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
                     }
